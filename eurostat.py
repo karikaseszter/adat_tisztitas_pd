c425a341ca3ec76c59acfc104e7ef40d0b3e839f
Eszter Karikás <karikaseszter82@gmail.com>
2024-10-01 19:45:35 +0200

    Manage multiple schemas

--- eurostat.py
+++ eurostat.py
@@ -1,40 +1,43 @@
 import numpy as np
 import pandas as pd
-
+from googletrans import Translator
 
 class EurostatTransformer:
-    def __init__(self, file_path, sheet_name=None):
-
+    def __init__(self, file_path, sheet_name=None, ):
         self.file_path = file_path
         self.sheet_name = sheet_name
         self.df = None
         self.transformed_df = None
+        self.translator = Translator()  # Fordító példány létrehozása
 
     def load_data(self):
-
         try:
+            # Adatok betöltése Excel fájlból
             self.df = pd.read_excel(self.file_path, sheet_name=self.sheet_name)
             print(f"Adatok betöltve a(z) '{self.sheet_name}' munkalapról.")
+
+            # Unnamed oszlopok eltávolítása
             self.df = self.df.loc[:, ~self.df.columns.str.contains('^Unnamed')]
+
+            # Hiányzó értékek kezelése
             self.df.replace(':', np.nan, inplace=True)
+
+            # Oszlopnevek eltávolítása
             self.df.columns = self.df.columns.str.strip()
 
-              #for index, row in self.df.head().iterrows():
-              #  print(f"Sor {index + 1}:")
-              #  for col_name in self.df.columns:
-              #      print(f"  {col_name}: {row[col_name]}")
-              #  print("-" * 40)
         except Exception as e:
             print(f"Hiba történt az adatok betöltésekor: {e}")
 
-    def transform_data(self):
+    def transform_data_v1(self, idvar, varname, valuename):
         if self.df is not None:
-            self.transformed_df = self.df.melt(id_vars=["GEO (Labels)"],
-                                               var_name="Évszám",
-                                               value_name="Az internet-hozzáférés szintje")
+            self.transformed_df = self.df.melt(id_vars=idvar,
+                                                var_name=varname,
+                                                value_name=valuename)
+            self.transformed_df = self.transformed_df.sort_values(by=idvar)
             print("Az adatok átalakítva.")
         else:
             print("Először be kell tölteni az adatokat.")
+
 
     def save_transformed_data(self):
         if self.transformed_df is not None:
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from eurostat import EurostatTransformer
 
 
 def main():
-    file_path = "data/Households - level of internet access.xlsx"
-    sheet_name = "Sheet 1"
-
-    eurostat = EurostatTransformer(file_path, sheet_name)
+    eurostat = EurostatTransformer("data/Households - level of internet access.xlsx", "Sheet 1")
     eurostat.load_data()
-    eurostat.transform_data()
+    eurostat.transform_data_v1("GEO (Labels)","Évek", "Az internet hozzáférés szintje")
     eurostat.save_transformed_data()
 
 
-if __name__ == "__main__":
+    eurostat = EurostatTransformer("data/Individuals - frequency of internet use.xlsx", "Sheet 1")
+    eurostat.load_data()
+    eurostat.transform_data_v1(["GEO", "INDIC_IS"], "Évek", "Az internet hozzáférés szintje")
+    eurostat.save_transformed_data()
+
+if __name__== "__main__":
     main()
