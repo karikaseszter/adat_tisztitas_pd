0df1374468231c9b1b4a8637610e44fe42e5a387
Eszter Karikás <karikaseszter82@gmail.com>
2024-10-07 19:47:19 +0200

    Translate function added

--- eurostat.py
+++ eurostat.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+from translate import Translator
 
 class EurostatTransformer:
-    def __init__(self, file_path, sheet_name=None, ):
+    def __init__(self, file_path, sheet_name=None):
         self.file_path = file_path
         self.sheet_name = sheet_name
         self.df = None
@@ -29,6 +30,25 @@ class EurostatTransformer:
         else:
             print("Először be kell tölteni az adatokat.")
 
+    def translate_text(self, column_name, target_lang='hu'):
+        try:
+            if self.transformed_df is not None:
+                translator = Translator(to_lang=target_lang)
+                self.transformed_df[column_name] = self.transformed_df[column_name].apply(lambda x: translator.translate(x) if pd.notnull(x) else x)
+
+                self.transformed_df[column_name].replace({
+                    "France": "Franciaország",
+                    "Ireland": "Írország",
+                    "Portugal": "Portugália",
+                    "European Union (27 countries)": "Európai Unió (27 ország)"
+
+                }, inplace=True)
+
+                print(f"A(z) '{column_name}' oszlop lefordítva {target_lang} nyelvre.")
+            else:
+                print("Először át kell alakítani az adatokat.")
+        except Exception as e:
+            print(f"Hiba történt a fordítás során: {e}")
 
     def save_transformed_data(self):
         if self.transformed_df is not None:
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from eurostat import EurostatTransformer
+from translate import Translator
 
+def translate_text(text, dest_lang='hu'):  # Example translating to Spanish (es)
+    try:
+        translated = Translator.translate(text, dest=dest_lang)
+        return translated.text
+    except Exception as e:
+        return str(e)
 
 def main():
-    eurostat = EurostatTransformer("data/Households - level of internet access.xlsx", "Sheet 1")
+    eurostat = EurostatTransformer("data/Households - level of internet access.xlsx", "Munka1")
     eurostat.load_data()
-    eurostat.transform_data_v1("GEO (Labels)","Évek", "Az internet hozzáférés szintje")
+    eurostat.transform_data_v1("GEO (Labels)","Évek", "Internet hozzáférés szintje")
+    eurostat.translate_text("GEO (Labels)")
     eurostat.save_transformed_data()
 
-
-    eurostat = EurostatTransformer("data/Individuals - frequency of internet use.xlsx", "Sheet 1")
+    eurostat = EurostatTransformer("data/Individuals - frequency of internet use.xlsx", "Munka1")
     eurostat.load_data()
-    eurostat.transform_data_v1(["GEO", "INDIC_IS"], "Évek", "Az internet hozzáférés szintje")
+    eurostat.transform_data_v1(["GEO (Labels)", "INDIC_IS (Labels)"], "Évek", "Internet használat gyakorisága")
+    eurostat.translate_text("GEO (Labels)")
+    eurostat.translate_text("INDIC_IS (Labels)")
     eurostat.save_transformed_data()
 
     eurostat = EurostatTransformer("data/Individuals - internet activities.xlsx", "Munka1")
     eurostat.load_data()
-    eurostat.transform_data_v1(["IND_TYPE", "INDIC_IS"], "Évek", "%")
+    eurostat.transform_data_v1(["IND_TYPE (Labels)", "INDIC_IS (Labels)"], "Évek", "%")
+    eurostat.translate_text("IND_TYPE (Labels)")
+    eurostat.translate_text("INDIC_IS (Labels)")
+    eurostat.save_transformed_data()
+
+    eurostat = EurostatTransformer("data/Individuals - devices used to access the internet.xlsx", "Munka1")
+    eurostat.load_data()
+    eurostat.transform_data_v1(["IND_TYPE (Labels)", "INDIC_IS (Labels)"], "Évek", "%")
+    eurostat.translate_text("IND_TYPE (Labels)")
+    eurostat.translate_text("INDIC_IS (Labels)")
     eurostat.save_transformed_data()
 
 if __name__== "__main__":
